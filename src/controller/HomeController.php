63535382d2d07660f2191f7e9c31f32539a56ebf
wf3 <alexandrebosc1@gmail.com>
2017-12-04 16:56:44 +0100

    Les débuts sont prometteurs
    Premier affichage de la page accueil grace à Silex

---
+++ src/controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace WF3\Controller;
+
+use Silex\Application;
+//cette ligne nous permet d'utiliser le service fourni par symfony pour gérer 
+
+class HomeController{
+
+	//page d'accueil qui affiche tout les articles
+	public function homePageAction(Application $app){
+		
+
+	 	return $app['twig']->render('index.html.twig');
+	}
+
+}
