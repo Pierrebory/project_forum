637017aef8611203f9e29e66e0f28e2f4563483a
norman33140 <norman33@live.fr>
2017-12-05 15:09:11 +0100

    Création de la vue du formulaire d'inscription

--- src/Form/Type/RegisterType.php
+++ src/Form/Type/RegisterType.php
@@ -7,15 +7,142 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
-class InscriptionType extends UserType
+class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildform($builder, $options);
-        $builder->remove('role');     
+        /////////////// NOM DE FAMILLE //////////////////
+        $builder->add('username', TextType::class, array(
+            'label' => 'Votre nom*',
+            'attr' => array(
+                'placeholder' => 'Au moins 2 caractères'
+            ),
+            'required' => true,            
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Length(array(
+                    'min' => 2,
+                    'max' => 45,
+                    'minMessage' => 'Votre nom doit faire au moins 2 caractères.',
+                    'minMessage' => 'Votre nom ne dois pas faire plus de 45 caractères.'
+                ))
+                new Assert\Regex(array(
+                    'pattern' => '/\d/',
+                    'match'   => false,
+                    'message' => 'Votre nom ne doit pas contenir de chiffres.'    
+                ))                
+            )
+        ));
+
+        /////////////////// PRENOM /////////////////////////
+        $builder->add('firstname', TextType::class, array(
+            'label' => 'Votre prénom*',
+            'attr' => array(
+                'placeholder' => 'Au moins 2 caractères'
+            ),
+            'required' => true,            
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Length(array(
+                    'min' => 2,
+                    'max' => 45,
+                    'minMessage' => 'Votre prénom doit faire au moins 2 caractères.',
+                    'minMessage' => 'Votre prénom ne dois pas faire plus de 45 caractères.'
+                ))
+                new Assert\Regex(array(
+                    'pattern' => '/\d/',
+                    'match'   => false,
+                    'message' => 'Votre prénom ne doit pas contenir de chiffres.'    
+                ))                  
+            )
+        ));   
+
+
+        /////////////// EMAIL //////////////////
+        $builder->add('email', TextType::class, array(
+            'label' => 'Votre email*',
+            'attr' => array(
+                'placeholder' => 'Au format exemple@domaine.com'
+            ),
+            'required' => true,            
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Email(array(
+                    'checkMX' => true,
+                    'message' => 'Votre adresse email n\'est pas valide.'                    
+                ))
+            )
+        ));   
+
+        ////////////////// MOT DE PASSE /////////////////////
+        $builder->add('password', RepeatedType::class, array(
+            'label' => 'Votre mot de passe*',           
+            'type' => PasswordType::class,
+            'attr' => array(
+                'placeholder' => 'Au moins 4 caractères'
+            ),            
+            'invalid_message' => 'Les mots de passe doivent être identiques.',
+            'required' => true,
+            'first_options'  => array('label' => 'Mot de passe'),
+            'second_options' => array('label' => 'Répeter le mot de passe'),
+            'constraints' => array(
+                new Assert\NotBlank(),
+                new Assert\Length(array(
+                    'min' => 4,
+                    'max' => 255,
+                    'minMessage' => 'Votre mot de passe doit faire au moins 4 caractères.',
+                    'minMessage' => 'Votre mot de passe ne dois pas faire plus de 255 caractères.'
+                ))
+            )            
+        ));    
+
+
+        ///////////// NUMERO DE TELEPHONE ///////////
+        $builder->add('phone', TextType::class, array(
+            'label' => 'Votre numéro de téléphone',
+            'attr' => array(
+                'placeholder' => 'Au format 0123456789'
+            ),           
+            'constraints' => array(
+                new Assert\Regex(array(
+                    'pattern' => '/[0][1-9][0-9]{8}/',
+                    'message' => 'Votre numéro de téléphone n\'est pas valide.'              
+                ))
+            )
+        ));   
+
+        ////////////////// VILLE ////////////////////
+        $builder->add('city', TextType::class, array(
+            'label' => 'Votre ville*',
+            'attr' => array(
+                'placeholder' => 'Au moins 2 caractères'
+            ),           
+            'required' => true,                
+            'constraints' => array(
+                new Assert\Regex(array(
+                    'pattern' => '/\d/',
+                    'match'   => false,
+                    'message' => 'Votre ville ne doit pas contenir de chiffres.'            
+                ))
+            )
+        ));   
+
+
+        $builder->add('role', ChoiceType::class, array(
+            'label' => 'Qui êtes-vous ?*',           
+            'attr' => array(
+                'name' => 'role',
+                'id' => 'role'
+            ),                  
+            'choices' => array(
+                'un élève' => 'ROLE_USER',
+                'un recruteur' => 'ROLE_ADMIN',
+            )
+        ));          
+
     }   
 
     public function getName()
--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -3,6 +3,8 @@ namespace WF3\Controller;
 
 use Silex\Application;
 //cette ligne nous permet d'utiliser le service fourni par symfony pour gérer 
+use WF3\Domain\User;
+use WF3\Form\Type\RegisterType;
 
 class HomeController{
 
@@ -33,4 +35,35 @@ class HomeController{
 
 	 	return $app['twig']->render('subject_forum.html.twig', array('subject' => $subject));
 	}
+
+	//////////// FORMULAIRE INSCRIPTION ////////////
+    public function registerAction(Application $app){
+		$user = new User();
+		$userForm = $app['form.factory']->create(RegisterType::class, $user);
+		// on envoie les paramètres de la requête à notre objet formulaire
+		$userForm->handleRequest($request); 
+		// si le formulaire a été envoyé
+		if($userForm->isSubmitted() && $userForm->isValid()){
+
+	        $salt = substr(md5(time()), 0, 23);
+	        $user->setSalt($salt);
+	        //on récupère le mot de passe en clair (envoyé par l'utilisateur)
+	        $plainPassword = $user->getPassword();
+	        // on récupère l'encoder de silex
+	        $encoder = $app['security.encoder.bcrypt'];
+	        // on encode le mdp
+	        $password = $encoder->encodePassword($plainPassword, $user->getSalt());
+	        //on remplace le mdp en clair par le mdp crypté
+	        $user->setPassword($password);
+
+		    $app['dao.user']->insert($user);				
+		    $app['session']->getFlashBag()->add('success', 'vous êtes bien enregistré');
+		    return $app->redirect($app['url_generator']->generate('home'));			
+		}
+
+		// j'envoi le formulaire
+		return $app['twig']->render('register.html.twig', array(
+			'userForm' => $userForm->createView(),
+		));		
+	}	
 }
