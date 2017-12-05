ac43cf0e6797bea5155e874d2e564f0239b642a8
norman33140 <norman33@live.fr>
2017-12-05 11:28:59 +0100

    Ajout du dossier form

---
+++ src/Form/Type/RegisterType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WF3\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class InscriptionType extends UserType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildform($builder, $options);
+        $builder->remove('role');     
+    }   
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
+
